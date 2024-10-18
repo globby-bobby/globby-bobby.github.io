743608505aedda9d4af994875d600500c17a7c01
Globby Bobby <supernageladam@icloud.com>
2024-10-18 14:15:55 -0600

    maze boundary generates (huge w)

--- arrayProject/sketch.js
+++ arrayProject/sketch.js
@@ -2,8 +2,14 @@
 // Adam S
 // Date
 //
+// Planes are 1000x1000 in size leading to poor render distance as everything is actually really, really large
+//
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Used WEBGL render for three-dimensional world
+// Used 'advanced algorithms' (like the maze gen)
+// Used images for textures
+// Used createCamera() and other camera related functionalities
+//
 
 let img;
 let cube1 = {};
@@ -21,8 +27,8 @@ const TURN_DEGREES = 90;
 const TURN_SPEED = 2.5;
 const MOVE_SPEED = 25;
 const MOVE_TIMES = 1000 / MOVE_SPEED;
-
-const MAZE_SIZE = 10;
+//must be odd
+const MAZE_SIZE = 30;
 
 const FRAMERATE = 60;
 
@@ -36,8 +42,10 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  //create camera and position it at origin
   cam = createCamera();
   cam.setPosition(0, 0, 0);
+  //cam.setPosition(0, -7000, 0);
   cam.lookAt(0, 0, -20);
   frameRate(FRAMERATE);
   //generateMaze();
@@ -49,7 +57,7 @@ function setup() {
 function draw() {
   orbitControl();
   background(0);
-  noStroke();
+  //noStroke();
   
   if (turningLeft || turningRight) {
     turnCamera();
@@ -63,6 +71,7 @@ function draw() {
 }
 
 function turnCamera() {
+  //check if the camera is still being told to turn for both directions
   if (leftTurns > TURN_DEGREES - 1) {
     leftTurns = 0;
     turningLeft = false;
@@ -73,6 +82,7 @@ function turnCamera() {
     turningRight = false;
     turningLeft = false;
   }
+  //turn camera and increase counter until it reaches TURN_DEGREES
   if (turningLeft && leftTurns < TURN_DEGREES) {
     leftTurns += TURN_SPEED;
     cam.pan(radians(TURN_SPEED),0,0);
@@ -84,12 +94,14 @@ function turnCamera() {
 }
 
 function moveCamera() {
+  //move camera by MOVE_SPEED and increase moves counter until it reaches MOVE_TIMES
   if (moves <= MOVE_TIMES - 1) {
     console.log(moves);
     cam.move(0,0,-MOVE_SPEED);
     moves++;
   }
   else {
+    //reset move counter and allow camera to move again
     moves = 0;
     moving = false;
   }
@@ -97,6 +109,7 @@ function moveCamera() {
 
 function checkInput() {
   if (keyIsPressed) {
+    //optimize having to check for three keys every frame, this could have been done in draw()
     if (keyIsDown(LEFT_ARROW) === true && !turningRight && !turningLeft && !moving) {
       turningLeft = true;
       turningRight = false;
@@ -114,12 +127,13 @@ function checkInput() {
 }
 
 function drawWorld() {
+  //run for every object in world array and "create" it
   for (let obj in world) {
     let object = world[obj];
     let size = object.size;
-
+    //since transformations are based on previous transformations, resetting the matrix allows individual position based on an origin
     resetMatrix();
-
+    //read attributes from object such as rotation, position, texture, and shape
     rotateX(radians(object.angleX));
     rotateY(radians(object.angleY));
     rotateZ(radians(object.angleZ));
@@ -138,8 +152,8 @@ function drawWorld() {
 }
 
 function generateMaze() {
-
-  let maze = [];
+  //generate maze for player to navigate
+  let maze = generateEdge();
   let height = 0;
   
   ground = {
@@ -155,16 +169,21 @@ function generateMaze() {
     shape: "plane",
   
   };
-
   maze.push(ground);
+  console.log(maze);
+  return maze;
+}
 
-  for (let times = 0; times < 15; times++) {
+function generateEdge() {
+  let maze = [];
+  //create top
+  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
 
     let object = {
       size: 1000,
-      x: round(random(-2,2)) * 1000,
+      x: times * 1000,
       y: 0,//round(random(-2,2)) * 1000,
-      z: -1000,
+      z: MAZE_SIZE/2 * -1000,
       angleX: 0,
       angleY: 0,
       angleZ: 0,
@@ -173,14 +192,46 @@ function generateMaze() {
     };
     maze.push(object);
   };
-
-  for (let times = 0; times < 15; times++) {
+  //create bottom
+  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
 
     let object = {
       size: 1000,
-      x: round(random(-2,2)) * 1000 - 500,
+      x: times * 1000,
       y: 0,//round(random(-2,2)) * 1000,
-      z: 1500,
+      z: MAZE_SIZE/2 * 1000,
+      angleX: 0,
+      angleY: 0,
+      angleZ: 0,
+      objectTexture: img,
+      shape: "plane",
+    };
+    maze.push(object);
+  };
+  //create left
+  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+
+    let object = {
+      size: 1000,
+      x: times * 1000,
+      y: 0,//round(random(-2,2)) * 1000,
+      z:  MAZE_SIZE/2 * -1000,
+      angleX: 0,
+      angleY: 90,
+      angleZ: 0,
+      objectTexture: img,
+      shape: "plane",
+    };
+    maze.push(object);
+  };
+  //create right
+  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+
+    let object = {
+      size: 1000,
+      x: times * 1000,
+      y: 0,//round(random(-2,2)) * 1000,
+      z:  MAZE_SIZE/2 * 1000,
       angleX: 0,
       angleY: 90,
       angleZ: 0,
@@ -190,6 +241,6 @@ function generateMaze() {
     maze.push(object);
   };
 
-  console.log(maze);
   return maze;
+
 }
