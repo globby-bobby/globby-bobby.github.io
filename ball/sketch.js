950c1985259081bd8e30bf0168fdfc94b76ac3a5
Globby Bobby <supernageladam@icloud.com>
2024-10-23 13:50:33 -0600

    Update sketch.js

--- ball/sketch.js
+++ ball/sketch.js
@@ -1,22 +1,22 @@
 
 let noiseNum;
 let time = 0;
-let size = 8;
+let imageSize = 8;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(700, 700);
   //noLoop();
 }
 
 function draw() {
   noStroke();
   background(220);
-  for (let x = 0; x < width; x += size){
+  for (let x = 0; x < width; x += imageSize){
     //level += 0.05;
-    for (let y = -5; y < height; y += size){
+    for (let y = -10; y < height; y += imageSize){
       console.log(noiseNum);
-      noiseNum = noise(x/200,y/200, frameCount/50);
-      square(x,y,size);
+      noiseNum = noise(x/500,y/500, frameCount/100);
+      square(x,y,imageSize);
       fill(noiseNum*150);
     
     }
