f296ebdbcf21070c3f35e67665dca5a7013c69f9
Globby Bobby <supernageladam@icloud.com>
2024-10-23 00:25:44 -0500

    Finished Maze Project

--- arrayProject/sketch.js
+++ arrayProject/sketch.js
@@ -1,23 +1,31 @@
 // Amazeing Maze
 // Adam S
-// Date
+// 2024/10/23
 //
 // Planes are 1000x1000 in size leading to poor render distance as everything is actually really, really large
 //
 // Extra for Experts:
 // Used WEBGL render for three-dimensional world
-// Used 'advanced algorithms' (like the maze gen)
 // Used images for textures
 // Used createCamera() and other camera related functionalities
+// Used pixelDensity() to optimize project by drawing less pixels
 //
+//--------------------------------------------------
+//
+//  Maze Navigation 'Game'
+//  Use WASD to move through the maze (s doesn't do anything)
+//  There is no collision because it would take too long (I'm stumped)
+//  There is no exit since the maze doesn't use any complex algorithm but just places walls randomly
+//
+//--------------------------------------------------
 
 let img;
-let cube1 = {};
-let cube2 = {};
+let img2;
+let img3;
 let world = [];
 let maze = [];
-let wallPositionArray0 = [];
-let wallPositionArray90 = [];
+//let wallPositionArray0 = [];
+//let wallPositionArray90 = [];
 let turningLeft = false;
 let turningRight = false;
 let leftTurns = 0;
@@ -33,35 +41,50 @@ const TURN_SPEED = 2.5;
 const MOVE_SPEED = 25;
 const MOVE_TIMES = 1000 / MOVE_SPEED;
 //must be odd
-const MAZE_SIZE = 9;
+let mazeSize;
 //i called it lazymaze because it rhymes, and because it just places walls at random.
 let LAZYMAZE_WALL_COUNT;
 
 const FRAMERATE = 60;
+const FREECAM = true;
+const PIXEL_DENSITY = 0.5;
 
 let cam;
 
-function preload() {
-  LAZYMAZE_WALL_COUNT = 1;// = round(MAZE_SIZE*(MAZE_SIZE/1.5));
-  img = loadImage("background.png");
+let finalImage;
 
+function preload() {
+  img = loadImage("background.png");
+  img2 = loadImage("background2.png");
+  img3 = loadImage("background3.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth,windowHeight, WEBGL);
+  pixelDensity(PIXEL_DENSITY);
+  //randomize maze size and make sure number is odd by adding 1 if number is even
+  mazeSize = round(random(7,22))
+  if (mazeSize % 2 === 0) {
+    mazeSize++;
+  }
+  LAZYMAZE_WALL_COUNT = round(mazeSize*(mazeSize/1.5));
+  //create Graphics object used to render window at a smaller resolution, then upscaled to show the final image on screen, for optimization
   //create camera and position it at origin
   cam = createCamera();
   cam.setPosition(0, 0, 0);
-  //cam.setPosition(0, -7000, 0);
+  //something to do with camera's field of view that makes turning feel better
+  cam.perspective(70);
   cam.lookAt(0, 0, -20);
+  //set framerate
   frameRate(FRAMERATE);
-  //generateMaze();
+  //create world
   world = generateMaze();
-
 }
 
 function draw() {
-  orbitControl();
+  if (FREECAM) {
+    orbitControl();
+  }
   background(0);
   noStroke();
   if (turningLeft || turningRight) {
@@ -70,7 +93,6 @@ function draw() {
   if (moving) {
     moveCamera();
   }
-  //console.log(round(cam.centerX - cam.eyeX));
   checkInput();
   drawWorld();
 }
@@ -101,48 +123,50 @@ function turnCamera() {
 function moveCamera() {
   //move camera by MOVE_SPEED and increase moves counter until it reaches MOVE_TIMES
   if (moves <= MOVE_TIMES - 1 ) {
-    if (frontIsBlocked()) {}
-    for (let item in world) {
-      let target = world[item];
-      if (target.x === round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 4)) {
-        //return;
-      }
-    }
+    // old collision detection
+    // if (frontIsBlocked()) {}
+    // for (let item in world) {
+    //   let target = world[item];
+    //   if (target.x === round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 4)) {
+
+    //   }
+    // }
     cam.move(0,0,-MOVE_SPEED);
-    //console.log(round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 2));
     moves++;
   }
   else {
     //reset move counter and allow camera to move again
     moves = 0;
-    //console.log(round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 2));
     moving = false;
   }
 }
 
-function frontIsBlocked() {
-  //player is moving along Y axis
-  if (round(cam.centerX - cam.eyeX) === 20 || round(cam.centerX - cam.eyeX) === -20) {
-    forwardY = round((cam.eyeY + MOVE_SPEED*MOVE_TIMES - 1000) / 2);
-    forwardX = cam.eyeX;
-    direction = "Y";
-  }
-  //moving along X axis
-  else{
-    forwardX = round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 2);
-    forwardY = cam.eyeY;
-    direction = "X";
-  }
-  console.log(direction);
-  for (let item in world) {
-    let target = world[item];
-    if (direction === "X") {
-      if (target.X + 500 === forwardX && target === forwardY) {
-        console.log("blocked");
-      }
-    }
-  }
-}
+// old collision detection
+//
+// function frontIsBlocked() {
+//   //player is moving along Y axis
+//   if (round(cam.centerX - cam.eyeX) === 20 || round(cam.centerX - cam.eyeX) === -20) {
+//     forwardY = round((cam.eyeY + MOVE_SPEED*MOVE_TIMES - 1000) / 100) * 200;
+//     forwardX = round(cam.centerX / 100) * 100;
+//     direction = "Y";
+//   }
+//   //moving along X axis
+//   else{
+//     forwardX = round((cam.eyeX + MOVE_SPEED*MOVE_TIMES - 1000) / 100) * 200;
+//     forwardY = round(cam.eyeY / 100) * 100;
+//     direction = "X";
+//   }
+//   console.log(direction);
+//   for (let item in world) {
+//     let target = world[item];
+//     //console.log(target.x,forwardX,target.y,forwardY,target.x === round(forwardX),target === forwardY);
+//     if (direction === "X") {
+//       if (target.x === forwardX && target.y === forwardY) {
+//         console.log("blocked");
+//       }
+//     }
+//   }
+// }
 
 function checkInput() {
   if (keyIsPressed) {
@@ -178,6 +202,13 @@ function drawWorld() {
     translate(object.x,object.y,object.z);
 
     texture(img);
+    //randomly flash different brightness of textures
+    if (round(random(0,10000)) === 75) {
+      texture(img2)
+    }
+    if (round(random(0,10000)) === 25) {
+      texture(img2)
+    }
 
     if (world[obj].shape === "plane") {
 
@@ -194,9 +225,9 @@ function generateMaze() {
   for (let times = 0; times < LAZYMAZE_WALL_COUNT; times++) {
     let object = {
       size: 1000,
-      x: round(random(-MAZE_SIZE/2, MAZE_SIZE/2)) * 1000,
+      x: round(random(-mazeSize/2, mazeSize/2)) * 1000,
       y: 0,
-      z: round(random(-MAZE_SIZE/2, MAZE_SIZE/2)) * 1000 - 500,
+      z: round(random(-mazeSize/2, mazeSize/2)) * 1000 - 500,
       angleX: 0,
       angleY: 0,
       angleZ: 0,
@@ -209,9 +240,9 @@ function generateMaze() {
   for (let times = 0; times < LAZYMAZE_WALL_COUNT; times++) {
     let object = {
       size: 1000,
-      x: round(random(-MAZE_SIZE/2, MAZE_SIZE/2)) * 1000,
+      x: round(random(-mazeSize/2, mazeSize/2)) * 1000,
       y: 0,
-      z: round(random(-MAZE_SIZE/2, MAZE_SIZE/2)) * 1000 - 500,
+      z: round(random(-mazeSize/2, mazeSize/2)) * 1000 - 500,
       angleX: 0,
       angleY: 90,
       angleZ: 0,
@@ -220,20 +251,29 @@ function generateMaze() {
     };
     maze.push(object);
   };
-
+  // old collision detection
+  // for (let item in maze) {
+  //   let target = maze[item];
+  //   if (target.angleY === 0) {
+  //     wallPositionArray0.push(target.x/4);
+  //   }
+  //   else{
+  //     wallPositionArray90.push(target.x/4);
+  //   }
+  // }
   return maze;
 }
 
 function generateEdge() {
   let maze = [];
   //create top
-  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+  for (let times = (mazeSize - 1)/-2; times < (mazeSize + 1)/2; times++) {
 
     let object = {
       size: 1000,
       x: times * 1000,
       y: 0,
-      z: MAZE_SIZE/2 * -1000,
+      z: mazeSize/2 * -1000,
       angleX: 0,
       angleY: 0,
       angleZ: 0,
@@ -243,13 +283,13 @@ function generateEdge() {
     maze.push(object);
   };
   //create bottom
-  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+  for (let times = (mazeSize - 1)/-2; times < (mazeSize + 1)/2; times++) {
 
     let object = {
       size: 1000,
       x: times * 1000,
       y: 0,
-      z: MAZE_SIZE/2 * 1000,
+      z: mazeSize/2 * 1000,
       angleX: 0,
       angleY: 0,
       angleZ: 0,
@@ -259,13 +299,13 @@ function generateEdge() {
     maze.push(object);
   };
   //create left
-  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+  for (let times = (mazeSize - 1)/-2; times < (mazeSize + 1)/2; times++) {
 
     let object = {
       size: 1000,
       x: times * 1000,
       y: 0,
-      z:  MAZE_SIZE/2 * -1000,
+      z:  mazeSize/2 * -1000,
       angleX: 0,
       angleY: 90,
       angleZ: 0,
@@ -275,13 +315,13 @@ function generateEdge() {
     maze.push(object);
   };
   //create right
-  for (let times = (MAZE_SIZE - 1)/-2; times < (MAZE_SIZE + 1)/2; times++) {
+  for (let times = (mazeSize - 1)/-2; times < (mazeSize + 1)/2; times++) {
 
     let object = {
       size: 1000,
       x: times * 1000,
       y: 0,
-      z:  MAZE_SIZE/2 * 1000,
+      z:  mazeSize/2 * 1000,
       angleX: 0,
       angleY: 90,
       angleZ: 0,
@@ -290,17 +330,18 @@ function generateEdge() {
     };
     maze.push(object);
   };
-  //check object x position and add it to wallPositionArray based on if they have an y angle of 90 or not
-  for (let item in maze) {
-    let target = maze[item];
-    if (target.angleY === 0) {
-      wallPositionArray0.push(target.x/4);
-    }
-    else{
-      wallPositionArray90.push(target.x/4);
-    }
-  }
-  console.log(wallPositionArray0);
+  //check object x position and add it to wallPositionArray based on if they have an y angle of 90 or not (old collision detection)
+  // for (let item in maze) {
+  //   let target = maze[item];
+  //   if (target.angleY === 0) {
+  //     wallPositionArray0.push(target.x/4);
+  //   }
+  //   else{
+  //     wallPositionArray90.push(target.x/4);
+  //   }
+  // }
+  // console.log(wallPositionArray0);
+  // console.log(wallPositionArray90);
   return maze;
 
 }
