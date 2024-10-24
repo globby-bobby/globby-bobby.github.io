a84876b8ac24f06f0c02dad8c10fffe637fa0d9c
Globby Bobby <supernageladam@icloud.com>
2024-10-24 12:14:12 -0600

    Created Game Project

---
+++ game/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let canvas;
+
+function setup() {
+  canvas = createCanvas(windowHeight, windowHeight);
+  canvas.position((windowWidth-windowHeight)/2,0);
+
+}
+
+function draw() {
+  background(220);
+}
