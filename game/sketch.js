a83f8d8e498dad01f6d62508c72eef29a4e7ad18
Globby Bobby <supernageladam@icloud.com>
2024-10-29 14:15:09 -0600

    Created Art For Game

--- game/sketch.js
+++ game/sketch.js
@@ -7,12 +7,24 @@
 
 let canvas;
 
+let tile;
+
+function preload() {
+  tile = loadImage('wall.png');
+}
+
 function setup() {
-  canvas = createCanvas(windowHeight, windowHeight);
-  canvas.position((windowWidth-windowHeight)/2,0);
+  canvas = createCanvas(windowHeight/3*4, windowHeight);
+  canvas.position((windowWidth-width)/2,0);
+  console.log(width,height);
 
 }
 
 function draw() {
-  background(220);
+  background(0,50,0);
+  for (let x = 0; x < width; x += width/16) {
+    for (let y = 0; y < height-height/8; y += width/16) {
+      image(tile,x,y,width/16,width/16);
+    }
+  }
 }
